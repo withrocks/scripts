62d503d8a2a40d3db5acd923a0c6f71bd69ab594
withrocks <withrocks@users.noreply.github.com>
2016-11-23 14:22:55 +0100

    Allow ignore list

--- debugging/test-with-trace/test-with-trace.py
+++ debugging/test-with-trace/test-with-trace.py
@@ -6,11 +6,12 @@ from test.unit.clarity_ext.dilution.test_dilutions import TestDilutionScheme
 import unittest
 import trace
 
-def run_test(name):
+def run_test(name, ignore):
     """Runs a unit test with tracing"""
     suite = unittest.TestLoader().loadTestsFromName(name)
     fn = lambda: unittest.TextTestRunner(verbosity=2).run(suite)
-    tracer = trace.Trace(ignoredirs=[sys.prefix, sys.exec_prefix],
+    # TODO: Ignore does not take full paths of modules!
+    tracer = trace.Trace(ignoremods=ignore, ignoredirs=[sys.prefix, sys.exec_prefix],
                          trace=1,
                          count=0)
     tracer.runfunc(fn)
@@ -18,6 +19,6 @@ def run_test(name):
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         test_name = sys.argv[1]
-    print("Running test: " + test_name)
-    run_test(test_name)
+    ignore = sys.argv[2].split(",") if len(sys.argv) > 2 else list()
+    run_test(test_name, ignore)
 
